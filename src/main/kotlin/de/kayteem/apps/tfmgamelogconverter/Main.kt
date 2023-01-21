ac8a8c3c1e8fd63f0087acbd3465131767b617b1
Tobias Mielke <kayteem@online.de>
2023-01-21 01:09:34 +0100

    Application now performs import operation in its parent directory - added import logic to main function

--- src/main/kotlin/de/kayteem/apps/tfmgamelogconverter/Main.kt
+++ src/main/kotlin/de/kayteem/apps/tfmgamelogconverter/Main.kt
@@ -1,5 +1,40 @@
 package de.kayteem.apps.tfmgamelogconverter
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import de.kayteem.apps.tfmgamelogconverter.controller.GameLogImporter
+import de.kayteem.apps.tfmgamelogconverter.controller.GameLogImporterImpl
+import de.kayteem.apps.tfmgamelogconverter.model.GameLog
+import java.nio.file.Path
+import java.nio.file.Paths
+
 fun main() {
-    println("Project Setup")
+
+    // build the importer
+    val objectMapper: ObjectMapper = jacksonObjectMapper()
+    val importer: GameLogImporter = GameLogImporterImpl(objectMapper)
+
+    // import all jsons from the execution directory
+    val executionPath: Path = Paths.get("").toAbsolutePath()
+    val gameLogs: List<GameLog> = importer.importAll(executionPath)
+    println("Imported ${gameLogs.size} json files")
+
+    // print results
+    gameLogs.forEach {
+        val timestamp = it.start
+        val board = it.board
+        val player1 = it.players.player1
+        val score1 = it.finalScores()[player1]
+        val player2 = it.players.player2
+        val score2 = it.finalScores()[player2]
+        val generations = it.generations()
+
+        println(
+            timestamp + ": " +
+            board + " - " +
+            player1.name + "[" + score1 + "] vs. " +
+            player2.name + "[" + score2 + "] - " +
+            generations + " generations"
+        )
+    }
 }
